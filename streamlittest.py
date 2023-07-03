357963ca4fc23b7483660ace06eb9b9931a7ba67
InvisibleTiger <jeevanadhya.vinothbabu@gmail.com>
2023-07-03 12:52:41 -0500

    add loading

--- streamlittest.py
+++ streamlittest.py
@@ -1,6 +1,14 @@
 import streamlit as st
-import search   
+import search
+import time   
 
 input = st.text_input('URL')
+
 if st.button('Summarize'):
-    st.text(search.summarize(input))
+    with st.spinner('Summarizing...'):
+        time.sleep(15)
+        print('Summarizing...')
+    st.success(search.summarize(input))
+    with st.spinner('Please wait for 60 seconds before you can summarize again...'):
+        time.sleep(60)
+        print('Please wait for 60 seconds before you can summarize again...')
